efd037379096029f0d86d3f92087225086daf524
Minjun An <41364201+YMGYM@users.noreply.github.com>
2023-02-06 14:13:46 +0900

    Update ubuntu-full-install.sh

--- ubuntu-full-install.sh
+++ ubuntu-full-install.sh
@@ -4,19 +4,19 @@ apt-get install zsh -y
 chsh -s $(which zsh)
 
 # symbolic link (same with ./install.sh)
-ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
-ln -s -f ~/.dotfiles/.zlogin ~/.zlogin
-ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
-ln -s -f ~/.dotfiles/.tmux/.tmux.conf ~/.tmux.conf
-ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -s -f "~/.dotfiles/.zshrc" "~/.zshrc"
+ln -s -f "~/.dotfiles/.zlogin" "~/.zlogin"
+ln -s -f "~/.dotfiles/.vimrc" "~/.vimrc"
+ln -s -f "~/.dotfiles/.tmux/.tmux.conf" "~/.tmux.conf"
+ln -s -f "~/.dotfiles/.tmux.conf.local" "~/.tmux.conf.local"
 
-cp -n ~/.dotfiles/.zshenv ~/.zshenv
-cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
+cp -n "~/.dotfiles/.zshenv" "~/.zshenv"
+cp -n "~/.dotfiles/.gitconfig" "~/.gitconfig"
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh --yes
 
 # install python3 to install autojump
 apt-get install python3 -y
-python3 ~/.dotfiles/plugins/autojump/install.py
+python3 "~/.dotfiles/plugins/autojump/install.py"
 
