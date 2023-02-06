b232432b7a4bb74439eaa4de3a8b22dd7949488c
Minjun An <41364201+YMGYM@users.noreply.github.com>
2023-02-06 13:53:28 +0900

    Update ubuntu-full-install.sh to absolute path

--- ubuntu-full-install.sh
+++ ubuntu-full-install.sh
@@ -1,15 +1,22 @@
 apt-get update
 # install zsh
-apt-get install zsh
+apt-get install zsh -y
 chsh -s $(which zsh)
 
-# flush dotfile 
-./install.sh
+# symbolic link (same with ./install.sh)
+ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/.dotfiles/.zlogin ~/.zlogin
+ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/.dotfiles/.tmux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+
+cp -n ~/.dotfiles/.zshenv ~/.zshenv
+cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh
 
 # install python3 to install autojump
-apt-get install python3
-cd ./plugins/autojump && ./install.py && cd -
+apt-get install python3 -y
+python3 ~/.dotfiles/plugins/autojump/install.py
 
